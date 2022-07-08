bdb4ec63915f904bba173779c0dd9e414b930640
haha505254 <haha505254@gmail.com>
2022-07-09 00:45:03 +0800

    first commit

---
+++ drf_message_board/messageboard/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Post
+from .serializers import PostSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
+  
